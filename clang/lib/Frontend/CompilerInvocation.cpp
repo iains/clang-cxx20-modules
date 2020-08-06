d8f914d6cbb52fb077490b182ee217d970ec71fa
Iain Sandoe <iain@sandoe.co.uk>
2020-08-06 12:13:04 +0100

    modules: Initial handling for -xc++-{system,user}-header.
    This defers any complaint about not finding a mentioned header until the search paths
    are setup.  We really need a 3-way action here (allowing the user to present a full path
    to either a system or user header).  Right now we expect filenames to be relative to some
    component in the search.

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -2026,6 +2026,7 @@ static InputKind ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
   }
 
   InputKind DashX(Language::Unknown);
+  bool IKIsSystemHeader = false;
   if (const Arg *A = Args.getLastArg(OPT_x)) {
     StringRef XValue = A->getValue();
 
@@ -2037,6 +2038,12 @@ static InputKind ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
                    XValue != "precompiled-header" &&
                    XValue.consume_back("-header");
 
+    bool IsPreprocessedHeader = Preprocessed && XValue.consume_back("-header");
+    // The user or system designation applies to both original source and the
+    // preprocessed output from those.
+    XValue.consume_back("-user");
+    IKIsSystemHeader = (IsHeaderFile || IsPreprocessedHeader) &&
+                       XValue.consume_back("-system");
     // Principal languages.
     DashX = llvm::StringSwitch<InputKind>(XValue)
                 .Case("c", Language::C)
@@ -2058,7 +2065,8 @@ static InputKind ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
                   .Default(Language::Unknown);
 
     // Some special cases cannot be combined with suffixes.
-    if (DashX.isUnknown() && !Preprocessed && !ModuleMap && !IsHeaderFile)
+    if (DashX.isUnknown() && !Preprocessed && !ModuleMap &&
+        !IsHeaderFile && !IsPreprocessedHeader)
       DashX = llvm::StringSwitch<InputKind>(XValue)
                   .Case("cpp-output", InputKind(Language::C).getPreprocessed())
                   .Case("assembler-with-cpp", Language::Asm)
@@ -2073,6 +2081,8 @@ static InputKind ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
 
     if (Preprocessed)
       DashX = DashX.getPreprocessed();
+    if (IsHeaderFile)
+      DashX = DashX.getHeader();
     if (ModuleMap)
       DashX = DashX.withFormat(InputKind::ModuleMap);
   }
@@ -2104,7 +2114,7 @@ static InputKind ParseFrontendArgs(FrontendOptions &Opts, ArgList &Args,
       IsSystem = Opts.IsSystemModule;
     }
 
-    Opts.Inputs.emplace_back(std::move(Inputs[i]), IK, IsSystem);
+    Opts.Inputs.emplace_back(std::move(Inputs[i]), IK, IsSystem || IKIsSystemHeader);
   }
 
   return DashX;
