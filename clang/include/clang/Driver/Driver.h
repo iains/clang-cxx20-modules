54d46f77ae9fde3e18e94eb037f7368b774d8bf0
Iain Sandoe <iain@sandoe.co.uk>
2020-08-20 16:18:57 +0100

    modules : Add fmodule-header, fmodule-header=
    These command-line flags are alternates to providing the -x
    c++-*-header indicators that we are building a header unit.
    
    Act on fmodule-header= for headers on the c/l:
    
    If we have x.hh -fmodule-header, then we should treat that header
    as a header unit input (equivalent to -xc++-header-unit-header x.hh).
    
    Likewise, for fmodule-header={user,system} the source should be now
    recognised as a header unit input (since this can affect the job list
    that we need).

--- clang/include/clang/Driver/Driver.h
+++ clang/include/clang/Driver/Driver.h
@@ -54,6 +54,17 @@ enum LTOKind {
   LTOK_Unknown
 };
 
+/// Whether headers used to construct C++20 module units should be looked
+/// up by the path supplied on the command line, or in the user or system
+/// search paths.
+enum ModuleHeaderMode {
+  HeaderMode_None,
+  HeaderMode_Default,
+  HeaderMode_User,
+  HeaderMode_System,
+  HeaderMode_Unknown
+};
+
 /// Driver - Encapsulate logic for constructing compilation processes
 /// from a set of gcc-driver-like command line arguments.
 class Driver {
@@ -80,6 +91,13 @@ class Driver {
     EmbedMarker,
     EmbedBitcode
   } BitcodeEmbed;
+
+  /// Header unit mode set by -fmodule-header={user,system}.
+  ModuleHeaderMode ModuleHeaderModeSet;
+
+  /// Set if we should process inputs and jobs with C++20 module
+  /// interpretation.
+  bool MaybeCXX20ModuleMode;
 
   /// LTO mode selected via -f(no-)?lto(=.*)? options.
   LTOKind LTOMode;
@@ -560,6 +578,12 @@ public:
 
   /// ShouldEmitStaticLibrary - Should the linker emit a static library.
   bool ShouldEmitStaticLibrary(const llvm::opt::ArgList &Args) const;
+
+  /// Returns true if the user has indicated a C++20 header unit mode.
+  bool hasHeaderMode() const { return ModuleHeaderModeSet != HeaderMode_None; }
+
+  /// Get the mode for handling headers as set by fmodule-header{=}.
+  ModuleHeaderMode getModuleHeaderMode () const { return ModuleHeaderModeSet; }
 
   /// Returns true if we are performing any kind of LTO.
   bool isUsingLTO(bool IsOffload = false) const {
