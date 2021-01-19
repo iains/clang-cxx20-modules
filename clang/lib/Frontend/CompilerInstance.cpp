9e736abbb06471d01c6a5a31b732ea52901caf55
Iain Sandoe <iain@sandoe.co.uk>
2021-01-19 13:57:29 +0000

    modules : Check for a module-mapper invocation.
    Don't try to create a mapper client/server pair unless the user
    made some invocation.

--- clang/lib/Frontend/CompilerInstance.cpp
+++ clang/lib/Frontend/CompilerInstance.cpp
@@ -2212,6 +2212,10 @@ void CompilerInstance::setExternalSemaSource(
 }
 
 ModuleClient *CompilerInstance::createMapper(SourceLocation Loc) {
+  /* Don't try unless the user asked for a module mapper.  */
+  if (getLangOpts().ModuleMapperInvocation.empty())
+    return nullptr;
+
   ModuleClient *M =
     ModuleClient::openModuleClient(Loc, getLangOpts().ModuleMapperInvocation,
                                    "clang");
