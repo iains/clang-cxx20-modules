10d30154d9da13ded5017566ad20cb031884f08a
Iain Sandoe <iain@sandoe.co.uk>
2021-01-28 13:15:56 +0000

    modules : Avoid writing AST when there is no Module.
    When we are emitting CMI, and there is no module definition
    that means we have no exports to emit.
    
    It is a bit of a stretch in places at the moment to determine
    the actions / outputs that need the CMI - but this should become
    less messy when we provide a mechanism for differentiating the
    interface build from the implementation where those are controlled
    by conditional compilation of the same source files.

--- clang/include/clang/Serialization/ASTWriter.h
+++ clang/include/clang/Serialization/ASTWriter.h
@@ -777,6 +777,7 @@ class PCHGenerator : public SemaConsumer {
   ASTWriter Writer;
   bool AllowASTWithErrors;
   bool ShouldCacheASTInMemory;
+  bool IsForCMI;
 
 protected:
   ASTWriter &getWriter() { return Writer; }
@@ -789,7 +790,7 @@ public:
                std::shared_ptr<PCHBuffer> Buffer,
                ArrayRef<std::shared_ptr<ModuleFileExtension>> Extensions,
                bool AllowASTWithErrors = false, bool IncludeTimestamps = true,
-               bool ShouldCacheASTInMemory = false);
+               bool ShouldCacheASTInMemory = false, bool IsForCMI = false);
   ~PCHGenerator() override;
 
   void InitializeSema(Sema &S) override { SemaPtr = &S; }
