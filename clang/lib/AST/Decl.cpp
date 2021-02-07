e03f673bba576432aee3059a1d131d9ead0ead44
Iain Sandoe <iain@sandoe.co.uk>
2021-02-07 01:00:33 +0000

    modules : Initial handling for module partitions.
    Works for simple cases.

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -1507,6 +1507,8 @@ Module *Decl::getOwningModuleForLinkage(bool IgnoreLinkage) const {
     return nullptr;
 
   case Module::ModuleInterfaceUnit:
+  case Module::ModulePartitionInterface:
+  case Module::ModulePartitionImplementation:
     return M;
 
   case Module::GlobalModuleFragment: {
